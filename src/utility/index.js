90610a83f9a59a139d576c2374f28538e4eabb69
tanfelixr <tanfelix.richard@gmail.com>
2024-04-02 17:43:41 +0800

    temp fix

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,43 +1,43 @@
 import { useQuizStore } from "@/store/quiz.js";
-const quizStore = useQuizStore();
+// const quizStore = useQuizStore();
 import { useUserStore } from "@/store/user.js";
-const userStore = useUserStore();
+// const userStore = useUserStore();
 
-let { achievementBadges } = userStore;
-let { difficulty } = quizStore;
+// let { achievementBadges } = userStore;
+// let { difficulty } = quizStore;
 
 export const finishQuizAnyDifficulty = () => {
-  if (difficulty === "easy") {
-    if (achievementBadges.finishTheQuizOnEasy.flag === 0) {
-      userStore.achievementBadges.finishTheQuizOnEasy.flag = 1;
-    }
-  } else if (difficulty === "medium") {
-    if (achievementBadges.finishTheQuizOnMedium.flag === 0) {
-      userStore.achievementBadges.finishTheQuizOnMedium.flag = 1;
-    }
-  } else if (difficulty === "hard") {
-    if (achievementBadges.finishTheQuizOnHard.flag === 0) {
-      userStore.achievementBadges.finishTheQuizOnHard.flag = 1;
-    }
-  }
+  //   if (difficulty === "easy") {
+  //     if (achievementBadges.finishTheQuizOnEasy.flag === 0) {
+  //       userStore.achievementBadges.finishTheQuizOnEasy.flag = 1;
+  //     }
+  //   } else if (difficulty === "medium") {
+  //     if (achievementBadges.finishTheQuizOnMedium.flag === 0) {
+  //       userStore.achievementBadges.finishTheQuizOnMedium.flag = 1;
+  //     }
+  //   } else if (difficulty === "hard") {
+  //     if (achievementBadges.finishTheQuizOnHard.flag === 0) {
+  //       userStore.achievementBadges.finishTheQuizOnHard.flag = 1;
+  //     }
+  //   }
 };
 
 export const finishQuizOnce = () => {
-  if (achievementBadges.finishTheQuizOnce.flag === 0) {
-    userStore.achievementBadges.finishTheQuizOnce.flag = 1;
-  }
+  //   if (achievementBadges.finishTheQuizOnce.flag === 0) {
+  //     userStore.achievementBadges.finishTheQuizOnce.flag = 1;
+  //   }
 };
 
 export const finishFlawlessly = () => {
-  if (userStore.lives === 3) {
-    if (achievementBadges.untouchable.flag === 0) {
-      userStore.achievementBadges.untouchable.flag = 1;
-    }
-  }
+  //   if (userStore.lives === 3) {
+  //     if (achievementBadges.untouchable.flag === 0) {
+  //       userStore.achievementBadges.untouchable.flag = 1;
+  //     }
+  //   }
 };
 
 export const checkForPoop = () => {
-  if (achievementBadges.zeroPointsOnSingleQuiz.flag === 0) {
-    userStore.achievementBadges.zeroPointsOnSingleQuiz.flag = 1;
-  }
+  //   if (achievementBadges.zeroPointsOnSingleQuiz.flag === 0) {
+  //     userStore.achievementBadges.zeroPointsOnSingleQuiz.flag = 1;
+  //   }
 };
