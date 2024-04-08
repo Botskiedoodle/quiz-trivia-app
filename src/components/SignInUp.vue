7cd222c1018f8bfea7f3b02f6ac1d17ed1c58872
tanfelixr <tanfelix.richard@gmail.com>
2024-04-08 14:36:24 +0800

    Sign in with username password working

--- src/components/SignInUp.vue
+++ src/components/SignInUp.vue
@@ -15,7 +15,7 @@
               <n-form-item-row label="Email Address">
                 <n-input
                   type="text"
-                  v-model="logInInfo.email"
+                  v-model:value="logInInfo.email"
                   placeholder="Enter Email Address"
                   maxlength="50"
                 />
@@ -23,14 +23,23 @@
               <n-form-item-row label="Password">
                 <n-input
                   type="password"
-                  v-model="logInInfo.password"
+                  v-model:value="logInInfo.password"
                   placeholder="Enter Password"
                   maxlength="20"
                   show-password-on="click"
                 />
               </n-form-item-row>
             </n-form>
-            <n-button type="primary" block secondary strong> Sign In </n-button>
+            <div v-if="errMsg">{{ errMsg }}</div>
+            <n-button
+              type="primary"
+              block
+              secondary
+              strong
+              @click="handleSignInWithEmailPassword"
+            >
+              Sign In
+            </n-button>
           </n-tab-pane>
           <n-tab-pane name="signup" tab="Sign up">
             <n-form>
@@ -73,27 +82,78 @@
 
 <script setup>
 import { defineModel, reactive, ref } from "vue";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "firebase/auth";
 import { useRouter } from "vue-router";
 const router = useRouter();
 const show = defineModel("show");
-const auth = getAuth();
+const errMsg = ref("");
 const logInInfo = ref({
   email: "",
   password: "",
   confirmPassword: ""
 });
 
-const handleRegisterWithEmailPassword = () => {
-  console.log(logInInfo.value);
-  createUserWithEmailAndPassword(
-    auth,
-    logInInfo.value.email,
-    logInInfo.value.password
-  ).then((response) => {
-    console.log("Successfully Signed In!");
-    router.push("/achievements");
-  });
+const handleRegisterWithEmailPassword = async () => {
+  const auth = getAuth();
+
+  try {
+    const res = await createUserWithEmailAndPassword(
+      auth,
+      logInInfo.value.email,
+      logInInfo.value.password
+    );
+    if (res) {
+      router.push("/achievements");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  // createUserWithEmailAndPassword(
+  //   auth,
+  //   logInInfo.value.email,
+  //   logInInfo.value.password
+  // ).then((response) => {
+  //   console.log("Successfully Signed In!");
+  //   router.push("/achievements");
+  // });
+};
+
+const errMessages = (err) => {
+  switch (err) {
+    case "auth/invalid-email":
+      errMsg.value = "Invalid email";
+      break;
+    case "auth/user-not-found":
+      errMsg.value = "No account with that email was found";
+      break;
+    case "auth/wrong-password":
+      errMsg.value = "Incorrect password";
+      break;
+    default:
+      errMsg.value = "Email or password is incorrect";
+      break;
+  }
+};
+
+const handleSignInWithEmailPassword = async () => {
+  const auth = getAuth();
+  try {
+    const res = await signInWithEmailAndPassword(
+      auth,
+      logInInfo.value.email,
+      logInInfo.value.password
+    );
+    if (res) {
+      router.push("/achievements");
+    }
+  } catch (error) {
+    console.log(error);
+    errMessages(error);
+  }
 };
 </script>
 
